587655d564dbd35c6684b2f59bb1f0b7b3651eb2
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-01-29 11:27:54 +0100

    make api return dummy content

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,89 @@
+const restaurants = [
+  {
+    id_: "1",
+    name: "restaurant 1",
+    img: "https://picsum.photos/500/600",
+    city_id: 1,
+    city_name: "Berlin",
+    comments: [
+      {
+        comment_id: 1,
+        text: "Comment 1",
+        date: Date.now,
+      },
+      {
+        comment_id: 2,
+        text: "Comment 2",
+        date: Date.now,
+      },
+      {
+        comment_id: 3,
+        text: "Comment 3",
+        date: Date.now,
+      },
+    ],
+    tags: [{ tag_id: 1, name: "beautiful" }],
+  },
+  {
+    id_: "2",
+    name: "restaurant 2",
+    img: "https://picsum.photos/400/600",
+    city_id: 2,
+    city_name: "Hamburg",
+    comments: [{ comment_id: 4, text: "Comment 1", date: Date.now }],
+    tags: [{ tag_id: 2, name: "fresh" }],
+  },
+  {
+    id_: "3",
+    name: "restaurant 3",
+    img: "https://picsum.photos/600/600",
+    city_id: 1,
+    city_name: "Berlin",
+    comments: [{ comment_id: 5, text: "Comment 1", date: Date.now }],
+    tags: [{ tag_id: 1, name: "beautiful" }],
+  },
+  {
+    id_: "4",
+    name: "restaurant 4",
+    img: "https://picsum.photos/400/600",
+    city_id: 2,
+    city_name: "Hamburg",
+    comments: [{ comment_id: 6, text: "Comment 1", date: Date.now }],
+    tags: [{ tag_id: 2, name: "fresh" }],
+  },
+  {
+    id_: "5",
+    name: "restaurant 5",
+    img: "https://picsum.photos/400/600",
+    city_id: 2,
+    city_name: "Hamburg",
+    comments: [{ comment_id: 1, text: "Comment 1", date: Date.now }],
+    tags: [{ tag_id: 1, name: "fresh" }],
+  },
+  {
+    id_: "5",
+    name: "restaurant 5",
+    img: "https://picsum.photos/400/600",
+    city_id: 2,
+    city_name: "Berlin",
+    comments: [{ comment_id: 1, text: "Comment 1", date: Date.now }],
+    tags: [{ tag_id: 2, name: "fresh" }],
+  },
+];
+const tags = [
+  { tag_id: 1, name: "beautiful" },
+  { tag_id: 2, name: "fresh" },
+];
+const cities = [
+  { city_id: 1, city_name: "Berlin" },
+  { city_id: 2, city_name: "Hamburg" },
+];
+
 const Api = {
-  getAllRestaurants: () => {},
-  getRestaurantById: (id) => {},
-  getRestaurantsFiltered: (search) => {},
-  getTags: () => {},
-  getCities: () => {},
+  getAllRestaurants: () => restaurants,
+  getRestaurantById: (id) =>
+    restaurants.find((retaurant) => (retaurant.id_ = id)),
+  getRestaurantsFiltered: (search) => restaurants.slice(0, 3),
+  getTags: () => tags,
+  getCities: () => cities,
 };
