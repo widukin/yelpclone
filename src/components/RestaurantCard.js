7a15840014cf353f71b4cc0d84fe9bc3868288b4
Hermann Marx <marxhermann@web.de>
2021-01-29 12:10:00 +0100

    added img, name, city and tags from the props called restaurant. Added Link to /restaurant.

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,4 +1,17 @@
-const RestaurantCard = () => {
-  return <>I am comments</>;
+import { Link } from "react-router-dom";
+
+const RestaurantCard = ({ restaurant }) => {
+  return (
+    <div>
+      <Link to={`/restaurants/${restaurant.id}`}>
+        <img src={restaurant.img} />
+        <p>{restaurant.name}</p>
+        <p>{restaurant.city_name}</p>
+        {restaurant.tags.map((tag, index) => {
+          return <p>{tag.name}</p>;
+        })}
+      </Link>
+    </div>
+  );
 };
 export default RestaurantCard;
