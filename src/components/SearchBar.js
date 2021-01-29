91f64bd2687208cac1c3afbd1f238979908e2cee
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-01-29 16:36:26 +0100

    add submitHandler which change url

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,32 +5,42 @@ const SearchBar = () => {
   const [value, setValue] = useState({tag: "", city: ""});
   const history = useHistory();
 
+  //TODO: trim user input
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+    if(value.tag && value.city) {
+      history.push("/tag=" + value.tag + "&city=" + value.city)
+    } else if (value.tag) {
+      history.push("/city=" + value.city)
+    } else if (value.city) {
+      history.push("/tag=" + value.tag)
+    } else {
+      history.push("/");
+    }
+    setValue({tag: "", city: ""});
+  };
 
   return (    
   <div className="searchbar">
     <form
       className="searchbar-form"
-      onSubmit={(e) => {
-        e.preventDefault();
-        value 
-        ? history.push("/tag=" + value.tag) : history.push("/");
-        setValue({tag: "", city: ""});
-      }}
-      onReset={() => setValue("")}
+      onSubmit={handleSubmit}
     >
       <input
         placeholder="Tag"
         className="searchbar-input"
         id="tag"
         value={value.tag}
-        onChange={(e) => setValue({tag: e.target.value.tag, city: value.city})}
+        onChange={(e) => setValue({tag: e.target.value, city: value.city})}
       />
       <input
         placeholder="City"
         className="searchbar-input"
         id="city"
         value={value.city}
-        onChange={(e) => setValue({tag: value.tag, city: e.target.value.city})}
+        onChange={(e) => setValue({tag: value.tag, city: e.target.value})}
       />
       <input className="searchbar-search" type="submit" />
     </form>
