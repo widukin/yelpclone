bde622b85dbc7d76b0d6d8f23423eef5f465a35b
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-01-30 12:59:47 +0100

    implement filter funcitonality for tag and city

--- src/views/ViewAllRestaurants.js
+++ src/views/ViewAllRestaurants.js
@@ -7,24 +7,63 @@ import RestaurantGrid from "../components/RestaurantGrid";
 import Api from "../api/index";
 
 const ViewAllRestaurants = () => {
-  const [restaurantsAll, setRestaurantsAll] = useState([]);
-  const { search } = useParams();
+  const [restaurantsList, setRestaurantsList] = useState([]);
+  const { tagId, cityId } = useParams();
+
 
   useEffect(() => {
+    console.log("tagId", tagId);
+    console.log("cityId", cityId);
 
-    search 
-    ? Api.getRestaurantsFiltered()
-      .then((res) => setRestaurantsAll(res))
+    Api.getAllRestaurants()
+    .then((res) => {
+      console.log(res);
+      let restaurantsFiltered = [];
+      
+      if (tagId || cityId) {
+        res.forEach(restaurant => {
+          if (restaurant.cityId) {
+            if (tagId === restaurant.tagId._id || cityId === restaurant.cityId._id) {
+              restaurantsFiltered.push(restaurant);
+            }
+          } else if (restaurant.city_id) {
+            if (tagId === restaurant.tagId._id || cityId === restaurant.city_id) {
+              restaurantsFiltered.push(restaurant);
+            }
+          }
+        });
+        console.log(restaurantsFiltered);
+        setRestaurantsList(restaurantsFiltered);
+      } else {
+        setRestaurantsList(res);
+      }
+    })
+    .catch((err) => {console.log("ERROR"); console.error(err)})
+
+/*     tagId
+    ? (
+
+      Api.getAllRestaurants()
+      .then((res) => {
+        let restaurantsByTag = [];
+        res.forEach(restaurant => {
+          if (tagId === restaurant.tagId._id) {
+            restaurantsByTag.push(restaurant);
+            setRestaurantsList(restaurantsByTag);
+          }
+        });
+      })
       .catch((err) => console.error(err))
+    )
     : Api.getAllRestaurants()
-      .then((res) => setRestaurantsAll(res))
-      .catch((err) => {console.error(err)})
-  }, [search]);
+      .then((res) => setRestaurantsList(res))
+      .catch((err) => {console.error(err)}) */
+  }, [tagId, cityId]);
 
 
   return(
     <>
-      <RestaurantGrid restaurants={restaurantsAll} />
+      <RestaurantGrid restaurants={restaurantsList} />
     </>
   );
 };
