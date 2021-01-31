358b56d3037fb33e2c81fef7cdabba71f3a5a05a
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-01-31 13:43:22 +0100

    adjustment of URL for filterning for tag and city

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,17 +3,19 @@ import { useHistory } from "react-router-dom";
 import Api from "../api";
 
 const SearchBar = () => {
-  const [tags, setTags] = useState([]);
-  const [cities, setCities] = useState([]);
-  const [value, setValue] = useState({tag: "", city: ""});
-  /* const history = useHistory(); */
+  const [tags, setTags] = useState([{_id: "", name: ""}]);
+  const [cities, setCities] = useState([{_id: "", name: ""}]);
+  const [value, setValue] = useState({tagName: "", cityName: ""});
+  const history = useHistory();
 
+  /* Get List of all Tags */
   useEffect(() => {
     Api.getTags("")
       .then((res) => {
         setTags(res);
       })
   }, []);
+  /* Get List of all Cities */
   useEffect(() => {
     Api.getCities("")
       .then((res) => {
@@ -25,17 +27,24 @@ const SearchBar = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(value);
-/*     if(value.tag && value.city) {
-      history.push("/tag=" + value.tag + "&city=" + value.city)
-    } else if (value.tag) {
-      history.push("/tag=" + value.tag)
-    } else if (value.city) {
-      history.push("/city=" + value.city)
+    let currentTag = {_id: "", name: ""};
+    let currentCity = {_id: "", name: ""};
+
+    // finds the current Tag and City object if available
+    value.tagName ? currentTag = (tags.find(tag => tag.name === value.tagName)) : console.log("no tag");
+    value.cityName ? currentCity = (cities.find(city => city.name === value.cityName)) : console.log("no city");
+
+    // adjusts URL if tag and city, both, only one or non is choosen
+    if(value.tagName && value.cityName) {
+      history.push("/tags/" + currentTag._id + "/cities/" + currentCity._id)
+    } else if (value.tagName) {
+      history.push("/tags/" + currentTag._id)
+    } else if (value.cityName) {
+      history.push("/cities/" + currentCity._id)
     } else {
       history.push("/");
-    } */
-    setValue({tag: "", city: ""});
+    }
+    setValue({tagName: "", cityName: ""});
   };
 
   return (    
@@ -49,8 +58,8 @@ const SearchBar = () => {
         className="searchbar-input"
         id="tag"
         list="tags"
-        value={value.tag}
-        onChange={(e) => setValue({tag: e.target.value, city: value.city})}
+        value={value.tagName}
+        onChange={(e) => setValue({tagName: e.target.value, cityName: value.cityName})}
       />
       <datalist id="tags">
         {
@@ -66,8 +75,8 @@ const SearchBar = () => {
         className="searchbar-input"
         id="city"
         list="cities"
-        value={value.city}
-        onChange={(e) => setValue({tag: value.tag, city: e.target.value})}
+        value={value.cityName}
+        onChange={(e) => setValue({tagName: value.tagName, cityName: e.target.value})}
       />
       <datalist id="cities">
         {
